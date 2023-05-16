24b430ca985ade9013d61c7fc6a1ab043d93736c
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-05-16 14:21:06 -0300

    first commit

---
+++ cadastro.py
@@ -0,0 +1,37 @@
+'''
+Aba para o Usuario fazer o cadastro.
+Requisitos: nome completo, nickname, email e senha.
+'''
+
+import os
+
+
+cadastro = input('Deseja fazer login?\n' )   
+login = ['a']
+senha_permitida = ['123']
+email = ['bb']
+
+while True:
+    if (cadastro == 'S' or cadastro == 's'): 
+        usuario = input('Úsuario: ')
+        senha_digitada = input('Senha: ')
+        email_digitado = input('Email: ')
+        
+    else:
+        print('Redericionando para o menu.')
+        break
+    if usuario in login or email_digitado in email:
+        print('Usuario já cadastrado.')
+
+    else:
+        print('Usuario não cadastrado.')
+        cas = input('Deseja cadastrar? ')
+
+        if cas == 's':
+            login.append(usuario)
+            senha_permitida.append(senha_digitada)
+            email.append(email_digitado)
+               
+        
+        else:
+             print('Redericionando para o menu.')
--- lista.py
+++ lista.py
@@ -1,22 +1,30 @@
 import os
 
 lista = []
+modo_op=input('Você deseja entrar no modo de operação? Digite [S] para sim e [N] para não.\n')
 while True:
-    print('Selecione uma opção')
-    opcao = input('[i] para inserir [a] para apagar [l] para listar: ')
-
+    if modo_op == 'S' or modo_op == 's':
+        print('Você entrou no modo de Operação.')
+        print('Selecione uma opção')
+        opcao = input('[i] para inserir [a] para apagar [l] para listar: ')
+    else:
+        print('Você selecionou a opção "Não", Favor voltar para o menu.')
+        break
+    
     if opcao == 'i':
         os.system('cls')
-        valor = input('Valor:')
+        valor = input('Digite um produto: ')
         lista.append(valor)
     elif opcao == 'a':
-        indice_str = input('Escolha o índice para apagar: ')
+        for i, valor in enumerate(lista):
+            print(i, valor, sep=' - ')
+        indice_str = input('Escolha o número do produto para apagar: ')
 
         try:
             indice = int(indice_str)
             del lista[indice]
         except:
-            print('Não foi possivel apagar este índice')
+            print('Não foi possivel apagar este produto.')
     elif opcao == 'l':
         os.system('cls')
 
@@ -24,6 +32,6 @@ while True:
             print('Lista vazia')
 
         for i, valor in enumerate(lista):
-            print(i, valor)
+            print(str(i+1), valor, sep=' - ')
     else:
         print('Opção inválida')
