0306ded0e3ba7985e97befcde3e8d8b7f0dbb1e2
Kauan Souza <kauansantosdesouza45@gmail.com>
2023-07-05 17:56:12 -0300

    First commit

--- login.py
+++ login.py
@@ -1,26 +1,33 @@
-import os
+# import os
 
 
-entrada = input('Você quer "Entrar" ou "Sair"? [E] para entrar e [S] para sair: ' )   
-login = 'kauanss'
-login_permitido = login
+# entrada = input('Você quer "Entrar" ou "Sair"? [E] para entrar e [S] para sair: ' )   
+# login = 'kauanss'
+# login_permitido = login
 
-senha_permitida = '123'
+# senha_permitida = '123'
 
-while True:
-    if (entrada == 'E' or entrada == 'e'): 
-        login = input('Digite o seu Úsuario: ')
-        senha_digitada = input('Senha: ')
+# while True:
+#     if (entrada == 'E' or entrada == 'e'): 
+#         login = input('Digite o seu Úsuario: ')
+#         senha_digitada = input('Senha: ')
         
 
-        if login == login_permitido and senha_digitada == senha_permitida:
-            print('Bem-Vindo!\nVocê entrou no sistema')
-            os.system('cls')
+#         if login == login_permitido and senha_digitada == senha_permitida:
+#             print('Bem-Vindo!\nVocê entrou no sistema')
+#             os.system('cls')
             
-        else:
-            print('Login ou a senha incorreto!')
-            break
+#         else:
+#             print('Login ou a senha incorreto!')
+#             break
  
-    else:
-        print('Você saiu do sistema') 
-        break
+#     else:
+#         print('Você saiu do sistema') 
+#         break
+
+
+import banco
+def usuario(name, password):
+
+    return banco.sql_query(f"""select count(*) from tabela_login where nome = '{name}' and senha = '{password}'""")
+    
