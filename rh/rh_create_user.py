82c3af127ef14d3fae28e094c2a8396bf0345793
Kauan <kauansantosdesouza45@gmail.com>
2023-10-08 20:15:24 -0300

    Alterado o banco de dados, planejando em alteração de permissão e adicionado o gitignore na aba geral

--- rh/rh_create_user.py
+++ rh/rh_create_user.py
@@ -1,83 +1,109 @@
 import rh_user
-from database import banco_server
+from database import banco
 import cx_Oracle
 from tkinter import Tk, Label, Button, Entry, messagebox
 
 
 def create_user(Windows1):
-    # Function that will have to collect the inserted information and send it to the inserted function 
+    # Function that will have to collect the inserted information and send it to the inserted function
     # that will insert the information in the employee table
     Windows1.destroy()
     Windows = Tk()
-    Windows.geometry('1440x1080')
+    Windows.geometry("1440x1080")
     Windows.title("Rh_Acesso")
     Windows.iconbitmap("imagens/rh.ico")
-    Windows.config(background='white')
-    
-    txt_tittle = Label(Windows, text="Preencha o Formulário", background="white", font=('Arial', 30))
-    txt_tittle.place(x=550, y=50 )
-    
-    txt_name = Label(Windows, text="Nome Completo", background="white", font=('Arial', 20))
+    Windows.config(background="white")
+
+    txt_tittle = Label(
+        Windows, text="Preencha o Formulário", background="white", font=("Arial", 30)
+    )
+    txt_tittle.place(x=550, y=50)
+
+    txt_name = Label(
+        Windows, text="Nome Completo", background="white", font=("Arial", 20)
+    )
     txt_name.place(x=100, y=200)
-    inbox_name = Entry(Windows, text="Login", font=('Arial',15), background='white')
-    inbox_name.place(x=100, y=250 )
-    
-    txt_mail = Label(Windows, text="Email", background="white", font=('Arial', 20))
+    inbox_name = Entry(Windows, font=("Arial", 15), background="white")
+    inbox_name.place(x=100, y=250)
+
+    txt_mail = Label(Windows, text="Email", background="white", font=("Arial", 20))
     txt_mail.place(x=400, y=200)
-    inbox_mail = Entry(Windows, font=('Arial',15), background='white')
+    inbox_mail = Entry(Windows, font=("Arial", 15), background="white")
     inbox_mail.place(x=400, y=250)
-    
-    txt_registration = Label(Windows, text="Mátricula", background="white", font=('Arial', 20))
+
+    txt_registration = Label(
+        Windows, text="Mátricula", background="white", font=("Arial", 20)
+    )
     txt_registration.place(x=700, y=200)
-    inbox_registration = Entry(Windows,  font=('Arial',15), background='white')
+    inbox_registration = Entry(Windows, font=("Arial", 15), background="white")
     inbox_registration.place(x=700, y=250)
-    
-    
-    txt_sector = Label(Windows, text="Setor", background="white", font=('Arial', 20))
+
+    txt_sector = Label(Windows, text="Setor", background="white", font=("Arial", 20))
     txt_sector.place(x=1000, y=200)
-    inbox_sector = Entry(Windows, font=('Arial',15), background='white')
+    inbox_sector = Entry(Windows, font=("Arial", 15), background="white")
     inbox_sector.place(x=1000, y=250)
-    
-    txt_office = Label(Windows, text="Cargo", background="white", font=('Arial', 20))
+
+    txt_office = Label(Windows, text="Cargo", background="white", font=("Arial", 20))
     txt_office.place(x=100, y=350)
-    inbox_office = Entry(Windows, font=('Arial',15), background='white')
+    inbox_office = Entry(Windows, font=("Arial", 15), background="white")
     inbox_office.place(x=100, y=400)
 
-    txt_login = Label(Windows, text="Login", background="white", font=('Arial', 20))
+    txt_login = Label(Windows, text="Login", background="white", font=("Arial", 20))
     txt_login.place(x=400, y=350)
-    inbox_login = Entry(Windows,  font=('Arial',15), background='white')
+    inbox_login = Entry(Windows, font=("Arial", 15), background="white")
     inbox_login.place(x=400, y=400)
-    
-    txt_password = Label(Windows, text="Senha", background="white", font=('Arial', 20))
+
+    txt_password = Label(Windows, text="Senha", background="white", font=("Arial", 20))
     txt_password.place(x=700, y=350)
-    inbox_password = Entry(Windows,  font=('Arial',15), background='white')
+    inbox_password = Entry(Windows, font=("Arial", 15), background="white")
     inbox_password.place(x=700, y=400)
 
-    txt_store = Label(Windows, text="Loja", background="white", font=('Arial', 20))
+    txt_store = Label(Windows, text="Loja", background="white", font=("Arial", 20))
     txt_store.place(x=1000, y=350)
-    inbox_store = Entry(Windows, font=('Arial',15), background='white')
+    inbox_store = Entry(Windows, font=("Arial", 15), background="white")
     inbox_store.place(x=1000, y=400)
-    
-    button_exe = Button(Windows, text='Executar', font=('Arial', 16), background='blue', fg='white', width=20)
-    button_exe["command"] = lambda name=inbox_name, mail = inbox_mail,  login = inbox_login, password = inbox_password, office = inbox_office, sector = inbox_sector,  registration = inbox_registration, store = inbox_store : inserted(name.get(),
-                                                                                                                                                                mail.get(),
-                                                                                                                                                                login.get(),
-                                                                                                                                                                password.get(),
-                                                                                                                                                                office.get(),
-                                                                                                                                                                sector.get(),
-                                                                                                                                                                registration.get(),                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
-                                                                                                                                                                store.get())
-    button_exe.place(x=650, y=600 )
-    button_back = Button(Windows, text='Voltar', font=('Arial', 16), background='blue', fg='white', width=20)
+
+    button_exe = Button(
+        Windows,
+        text="Executar",
+        font=("Arial", 16),
+        background="blue",
+        fg="white",
+        width=20,
+    )
+    button_exe[
+        "command"
+    ] = lambda name=inbox_name, mail=inbox_mail, login=inbox_login, password=inbox_password, office=inbox_office, sector=inbox_sector, registration=inbox_registration, store=inbox_store: inserted(
+        name.get(),
+        mail.get(),
+        login.get(),
+        password.get(),
+        office.get(),
+        sector.get(),
+        registration.get(),
+        store.get(),
+    )
+    button_exe.place(x=650, y=600)
+    button_back = Button(
+        Windows,
+        text="Voltar",
+        font=("Arial", 16),
+        background="blue",
+        fg="white",
+        width=20,
+    )
     button_back["command"] = lambda button_back=button_back: open_user(Windows)
-    button_back.place(x=650, y=650 )
-    
+    button_back.place(x=650, y=650)
+
+
 def open_user(self):
     rh_user.user(self)
-    
-def inserted(name, mail, login, password, office, sector, registration,store):
+
+
+def inserted(name, mail, login, password, office, sector, registration, store):
     try:
-        banco_server.sql_server_insert(f"""INSERT INTO tb_funcionarios(
+        banco.sql_inserir(
+            f"""INSERT INTO tb_funcionarios(
                                 NOME_COMPLETO,
                                 EMAIL,
                                 USUARIO,
@@ -96,9 +122,12 @@ def inserted(name, mail, login, password, office, sector, registration,store):
                                     '{sector.upper()}',
                                     '{registration.upper()}',
                                     '{store.upper()}')
-                            """)
+                            """
+        )
         messagebox.showinfo("Aviso!", "Usuário Criado com Sucesso!")
-    except cx_Oracle.DatabaseError as e :
-                error, = e.args
-                messagebox.showerror("Erro no Banco de Dados", f"Ocorreu um erro no banco de dados:\n{error.message}")
-
+    except cx_Oracle.DatabaseError as e:
+        (error,) = e.args
+        messagebox.showerror(
+            "Erro no Banco de Dados",
+            f"Ocorreu um erro no banco de dados:\n{error.message}",
+        )
--- rh/rh_panel.py
+++ rh/rh_panel.py
@@ -1,37 +1,56 @@
-from database import banco_server
+from database import banco
 from rh_image import pay_payslip
 from rh_user import user
 from tkinter import Tk, Label, Button, messagebox
 
-def main_panel(Windows1, login = '', senha=''):
-        # After logging in, you will go to the menu screen where there are 2 buttons, 
-        # We have the "Employees" button where we can create or modify the user and the "Launch Payslip" button, having to put the requested information and launch the images
-        flag= False
-        
-        try:
-            result = banco_server.sql_query(f"""SELECT count(*) FROM tb_funcionarios WHERE usuario = '{login.get().upper()}' and senha = '{senha.get().upper()}'""")
-            if result[0][0] == 1:
-                flag = True
-        except:
-            flag= True
-        if flag:
-            Windows1.destroy()
-            Windows = Tk()
-            Windows.geometry('1440x1080')
-            Windows.title("Rh_Acesso")
-            Windows.iconbitmap("imagens/rh.ico")
-            Windows.config(background='white')
-            
-            txt_tittle = Label(Windows, text="Painel Principal", background="white", font=('Arial', 30))
-            txt_tittle.pack(padx=10, pady=10)
-            
-            button_image = Button(Windows, text='Lançar Holerite', font=('Arial', 20), background='blue', fg='white', width=20,)
-            button_image["command"] = lambda button_image=button_image: pay_payslip(Windows)
-            button_image.pack(padx=10, pady=50)
-            
-            button_create_user = Button(Windows, text='Funcionário', font=('Arial', 20), background='blue', fg='white', width=20,)
-            button_create_user["command"] = lambda button_image=button_image: user(Windows)
-            button_create_user.pack(padx=10, pady=50)
-        
-        else:
-            messagebox.showerror("Erro!", "Login ou Senha incorretos")
+
+def main_panel(Windows1, login="", senha=""):
+    # After logging in, you will go to the menu screen where there are 2 buttons,
+    # We have the "Employees" button where we can create or modify the user and the "Launch Payslip" button, having to put the requested information and launch the images
+    flag = False
+
+    try:
+        result = banco.sql_query(
+            f"""SELECT count(*) FROM tb_login WHERE login = '{login.upper()}' and senha = '{senha.upper()}'"""
+        )
+        if result[0][0] == 1:
+            flag = True
+    except:
+        flag = True
+    if flag:
+        Windows1.destroy()
+        Windows = Tk()
+        Windows.geometry("1440x1080")
+        Windows.title("Rh_Acesso")
+        Windows.iconbitmap("imagens/rh.ico")
+        Windows.config(background="white")
+
+        txt_tittle = Label(
+            Windows, text="Painel Principal", background="white", font=("Arial", 30)
+        )
+        txt_tittle.pack(padx=10, pady=10)
+
+        button_image = Button(
+            Windows,
+            text="Lançar Holerite",
+            font=("Arial", 20),
+            background="blue",
+            fg="white",
+            width=20,
+        )
+        button_image["command"] = lambda button_image=button_image: pay_payslip(Windows)
+        button_image.pack(padx=10, pady=50)
+
+        button_create_user = Button(
+            Windows,
+            text="Funcionário",
+            font=("Arial", 20),
+            background="blue",
+            fg="white",
+            width=20,
+        )
+        button_create_user["command"] = lambda button_image=button_image: user(Windows)
+        button_create_user.pack(padx=10, pady=50)
+
+    else:
+        messagebox.showerror("Erro!", "Login ou Senha incorretos")
